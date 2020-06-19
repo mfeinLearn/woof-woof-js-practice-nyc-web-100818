ec7c2a20d6a3dacb8ac9ccf52b3c27848c1485b8
github username <github email address>
2020-06-19 04:01:57 -0400

    added NOTES.md file

--- src/index.js
+++ src/index.js
@@ -22,20 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         dogBar.innerHTML = str
 
-        //---***other way - the appending way***---
-        // if you are creating elements you need to append child
-        // if you are creating a string you need to set the element that
-        //.. is a string to the inner html of id of the element this case
-        //.. it is the id of the div ^^seen above^^
-
-        // const dogBar = document.querySelector(`#dog-bar`)
-        // arr.forEach(dog => {
-        //     let span = document.createElement("span")
-        //     span.innerHTML = dog.name
-        //     dogBar.appendChild(span)
-        // })
-                //---***other way***---
-
     }
 
 })
